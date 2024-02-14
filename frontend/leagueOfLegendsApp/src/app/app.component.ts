5d70867744513af0ddc7a2ed0fec8d3fcfce26b2
Gerardoicu <gerardoicu@gmail.com>
2024-02-13 23:47:22 -0600

    initialize carousel

--- frontend/leagueOfLegendsApp/src/app/app.component.ts
+++ frontend/leagueOfLegendsApp/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import {CarouselComponent} from "./globalComponents/carousel/carousel.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
+  imports: [
+    CarouselComponent
+  ],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements  OnInit{
--- frontend/leagueOfLegendsApp/src/app/globalComponents/carousel/carousel.component.ts
+++ frontend/leagueOfLegendsApp/src/app/globalComponents/carousel/carousel.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { gsap, TweenMax, Power2 } from 'gsap';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   standalone: true,
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
+  @ViewChild('carousel', { static: true }) carousel!: ElementRef<HTMLDivElement>;
 
+  images: string[] = [
+    'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lux_39.jpg',
+    'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lux_38.jpg',
+    'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lux_42.jpg'
+  ];
+
+  ngOnInit(): void {
+    this.setupCarousel();
+  }
+
+  setupCarousel(): void {
+    gsap.to(this.carousel.nativeElement, {
+      xPercent:50 * (this.images.length - 1),
+      ease: Power2.easeInOut,
+      repeat: -1,
+      duration: 5,
+    });
+
+  }
 }
